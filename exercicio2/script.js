893e5bf977c74e98e90dfb92637451bf0d1fa01b
Giovanni Souza <GSMartins999>
2023-05-11 20:30:21 -0300

    exerciciosstrings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,32 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// # Exercício 2
+
+// Observe a string abaixo:
+
+// ```jsx
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// ```
+
+// A partir dela, execute os passos abaixo:
+
+// a) **Remova** o excesso de espaços no final da string;
+
+// b) Imprima no console a **quantidade de caracteres** da string, *antes* e *depois* da remoção dos espaços;
+
+// c) **Substitua** os traços `________` por “sticioso”.
+
+const minhaString = "     Eu não sou supersticioso, mas sou um pouco ________.";
+let removerEspacos = minhaString.trim()
+console.log(removerEspacos.trim())
+console.log(minhaString.length)
+console.log(removerEspacos.replace("________","sticioso"))
+console.log(removerEspacos.length)
+
+
+
+
+
+
+
+
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,22 @@
-//Crie a const para a frase aqui
+// //Crie a const para a frase aqui# Exercício 3
+
+// A partir da frase a seguir, faça o que se pede.
+
+// ```
+// Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+// ```
+
+// a) Crie uma `const` no seu código para **guardar a frase** (com aspas e tudo);
+
+// b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+
+// c) **Verifique** se a nova string inclui **verde**, e se inclui **laranja**.
+
+// **EXTRA:** tente fazer o trecho “mas não deixe o gato sair” ficar em **letras maiúsculas**, assim como o “BOAS VINDAS”
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: " + "BOAS VINDAS, mas não deixe o gato sair".toUpperCase("mas não deixe o gato sair")
+console.log(frase)
+const troca = frase.replaceAll("verde","rosa").replaceAll("azul", "laranja")
+console.log(troca)
+const verifica = troca.includes("verde" && "laranja")
+console.log(verifica)
